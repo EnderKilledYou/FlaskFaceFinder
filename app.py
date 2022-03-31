2e7ade6434a5798dba5b42c81dd7e0c38bdf6a3e
EnderKilledYou <92962648+EnderKilledYou@users.noreply.github.com>
2022-03-31 14:42:04 -0500

    Update app.py
    push update for lazy init for some versionf os python.

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///face.sqlite3'
 app.config['SECRET_KEY'] = 'E315277807427FFC1CAF0B1040CEC8E84BE32F482377A42290AA301CCD896726'
 db = SQLAlchemy(app)
 
-from auth import auth as auth_blueprint
-from main import main as main_blueprint
-from image_processing import image_processing as image_processing_blueprint
-from orm import User, UserImage
 
-from images import images as images_blueprint
+def lazy_init:
+    from auth import auth as auth_blueprint
+    from main import main as main_blueprint
+    from image_processing import image_processing as image_processing_blueprint
+    from orm import User, UserImage
+    from images import images as images_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(images_blueprint)
+    app.register_blueprint(image_processing_blueprint)
 
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(main_blueprint)
-app.register_blueprint(images_blueprint)
-app.register_blueprint(image_processing_blueprint)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-db.init_app(app)
-db.create_all()
+    db.init_app(app)
+    db.create_all()
 
 
 @login_manager.user_loader
@@ -32,4 +32,5 @@ def load_user(user_id):
 
 
 if __name__ == '__main__':
+    lazy_init()
     app.run(allow_extra_args=True)
