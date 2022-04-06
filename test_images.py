fb3d1c95eaf6b9a2e2713de2a6721141e666381d
maidmariandev <59107019+runacode@users.noreply.github.com>
2022-04-06 18:14:15 -0500

    testing for delete

--- test_images.py
+++ test_images.py
@@ -25,6 +25,33 @@ def test_image_upload():
     assert response2.status_code == 200
 
 
+def test_image_delete():
+    client = app.test_client()
+    with open("testimage.base64", "r") as data_file:
+        base64_content = data_file.read()
+        upload_data = json.dumps({'image': base64_content})
+
+    response = client.post('/login',
+                           data=json.dumps({'email': 'someg8ggbggg6g85gv4g77@place.com',
+                                            'password': 'abcd123applebanana'}),
+                           content_type='application/json', )
+    data = response.json
+
+    assert response.status_code == 200
+
+    response2 = client.post('/upload_image',
+                            data=upload_data,
+                            content_type='application/json', )
+
+    assert response2.status_code == 200
+
+    data2 = response2.json
+
+    response3 = client.get('/delete_image/' + str(data2['id']))
+
+    assert response3.status_code == 200
+
+
 def test_image_get():
     client = app.test_client()
     with open("testimage.base64", "r") as data_file:
